2ba75ec6467ef882025a762a52e960cd2d25f1ff
Your Name <you@example.com>
2024-04-16 18:18:24 -0400

    main.py modified adding merge.yml

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ now = datetime.now()
 try:
     g = Github(os.environ["GITHUB_TOKEN"])
     repo = g.get_repo(os.environ['REPO_NAME'])
-    print(" hello from docker")
-    pull = repo.get_pulls(state='open')
-    print("hello from docker")
-    print(f"{repo}")
+    pulls = repo.get_pulls(state='open')
+    pr_number = int(os.environ['PR_NUMBER']) if (os.environ['PR_NUMBER']) else None
+    pr = repo.get_pull(pr_number) if (pr_number) else None
+    MERGE_PR = os.environ.get("MERGE_PR")
+    CLOSE_PR = os.environ.get("CLOSE_PR")
     msg = {
         # 1 stale PR
         "stale_label" : 'This PR is stale because it has been open 15 days with no activity. Remove stale label or comment/update PR otherwise this will be closed in next 2 days.' ,
@@ -18,6 +19,23 @@ try:
         # 2.close staled PR if 2 days of no activity
         "staled_PR_closing" : 'This PR was closed because it has been stalled for 2 days with no activity.' ,
         # 3.Check if the pull request targets the master branch directly
+        "check_PR_target": 'Do not accept PR target from feature branch to master branch.',
+        # 4.Check if the pull request has a description
+        "check_description": 'No Description on PR body. Please add valid description.',
+        # 5_1 Check if the Approved comment in the pull request comments
+        "approve_merge": 'Pull Request Approved and Merged!',
+        "approve_comment": 'This pull request was approved and merged because of a slash command.',
+        # 5_2 Check if the Close comment in the pull request comments
+        "closing_comment": 'This pull request was closed because of a slash command.',
+        # 6. Check All the files and see if there is a file named "VERSION"
+        "check_version_file": 'The VERSION file exists. All ohk',
+        "version_file_inexistence": "The VERSION file does not exist. Closing this pull request.",
+        # 7. Check if version name from "VERSION" already exists as tag
+        "tagcheck_success": "The VERSION didnt matched with tag. All ok",
+        "tagcheck_reject": "The tag from VERSION file already exists. Please update the VERSION file.",
+        # 8. Close the PR having DO NOT MERGE LABEL
+        "label": "Please remove DO NOT MERGE LABEL",
+        # 9. message need to be placed here
         }
     for pull in pulls:
         time_diff = now - pull.updated_at
@@ -33,6 +51,20 @@ try:
                 print(msg.get("staled_PR_closing"))
                 pull.edit(state="closed")
                 pull.create_issue_comment(msg.get("staled_PR_closing") )
+    
+    if MERGE_PR.__eq__('true'):
+        if pr:    
+            pr.merge(merge_method = 'merge', commit_message = msg.get("approve_merge"))
+            pr.create_issue_comment(msg.get("approve_comment"))
+            print(msg.get("approve_comment"))
 
+    # 5_2 Check if the Close comment in the pull request comments
+    if CLOSE_PR.__eq__('true'):
+        if pr:            
+            pr.edit(state="closed")
+            pr.create_issue_comment(msg.get("closing_comment"))
+            print(msg.get("closing_comment"))
+
+     
 except Exception as e:
-    print(f"Failed to run the job. exception: {str(e)}")  
+    print(f"Failed to run the job. exception: {str(e)}")
