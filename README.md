04779d46f4876bc8b4c35421d55ea69194f37bbf
Your Name <you@example.com>
2024-04-19 20:19:04 -0400

    version 2.3.0.0

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Python Docker Container Actions
+# GitHub Actions Workflow for PR Management using Docker,Python ,Bash script
+
+This GitHub Actions workflow automates various tasks related to pull request (PR) management, such as checking for stale PRs, enforcing certain rules, and responding to slash commands.
+
+## Workflow Overview
+
+The workflow is triggered by different events, including stale PR checks, pull request updates, and slash commands. It performs a series of actions based on the event type and the configured rules.
+
+## Workflow Components
+
+### 1. **Environment Setup**
+   - Uses GitHub token for authentication.
+   - Retrieves necessary environment variables like repository name, PR number, event type, etc.
+
+### 2. **Messages for Google Chat Notifications**
+   - Defines messages to be sent to Google Chat for various events such as stale PRs, PR closures, approvals, etc.
+
+### 3. **Event Handling**
+   - Handles events based on the workflow type (`stale`, `pull`, `slash`).
+   - Checks for stale PRs, enforces rules for PR targets and descriptions, checks for existing tags, and responds to slash commands.
+
+### 4. **Google Chat Integration**
+   - Sends notifications to Google Chat based on the event and associated actions.
+
+### 5. **Error Handling**
+   - Catches and prints any exceptions that occur during workflow execution.
+
+## Usage
+
+To use this workflow in your repository:
+1. Copy the provided code into a `.github/workflows` directory in your repository.
+2. Configure the required environment variables such as `GITHUB_TOKEN`, `REPO_NAME`, `WEBHOOK`, etc., either in GitHub Secrets or directly in the workflow file.
+3. Customize the workflow as needed, such as adjusting the stale PR days, specifying message templates, etc.
+
+## Notes
+
+- Make sure to review and test the workflow thoroughly before enabling it in your repository.
+- Ensure that the necessary permissions are granted to the GitHub token used by the workflow.
+- Update the workflow and messages according to your specific requirements and workflow policies.
--- main.py
+++ main.py
@@ -4,181 +4,140 @@ from datetime import datetime, timedelta
 import requests
 
 try:
-    # env values
+    # Get GitHub token from environment variable
     g = Github(os.environ["GITHUB_TOKEN"])
+    
+    # Get repository and other environment variables
     repo = g.get_repo(os.environ['REPO_NAME'])
     repo_name=os.environ['REPO_NAME']
-    print("repo_name ={repo_name}")
     pulls = repo.get_pulls(state='open')
-
+    GCHAT_MESSAGE=[]
     pr_number = int(os.environ['PR_NUMBER']) if ( os.environ['PR_NUMBER'] ) else None
     pr = repo.get_pull(pr_number) if(pr_number) else None    
-
     MERGE_PR = os.environ.get("MERGE_PR")
     CLOSE_PR = os.environ.get("CLOSE_PR")
     VERSION_FILE = os.environ.get("VERSION_FILE")
     EVENT = os.environ['EVENT']
     GCHAT_WEBHOOK_URL = os.environ['WEBHOOK']
-    print(f"print gchat token {GCHAT_WEBHOOK_URL}")
+    EVENT_CHECK=os.environ['EVENT_CHECK_VARIABLE']
+     
+     # Fuction to send the message to GCHAT
+    def send_message_to_google_chat(message, webhook_url):
+        payload = {"text": message}
+        response = requests.post(webhook_url, json=payload)
+        return response
 
+    # Define messages for Google Chat and comment body in Github
     msg = {
-        # 1 stale PR 
-        "stale_label" : 'This PR is stale because it has been open 15 days with no activity. Remove stale label or comment/update PR otherwise this will be closed in next 2 days.' ,
-        "stale_days" : 15,
-        "stale_close_days" : 2,
-        # 2.close staled PR if 2 days of no activity
-        "staled_PR_closing" : 'This PR was closed because it has been stalled for 2 days with no activity.' ,
-        # 3.Check if the pull request targets the master branch directly
-        "check_PR_target" : 'Do not accept PR target from feature branch to master branch.' ,
-        # 4.Check if the pull request has a description
-        "check_description" : 'No Description on PR body. Please add valid description.' ,
-        # 5_1 Check if the Approved comment in the pull request comments
-        "approve_merge" : 'Pull Request Approved and Merged!' ,
-        "approve_comment" : 'This pull request was approved and merged because of a slash command.' ,
-        # 5_2 Check if the Close comment in the pull request comments
-        "closing_comment" : 'This pull request was closed because of a slash command.' ,
-        # 6. Check All the files and see if there is a file named "VERSION"
-        "check_version_file" : 'The VERSION file exists. All ohk' ,
-        "version_file_inexistence" : "The VERSION file does not exist. Closing this pull request." ,
-        # 7. Check if version name from "VERSION" already exists as tag  
-        "tagcheck_success" : "The VERSION didnt matched with tag. All ok" ,
-        "tagcheck_reject" : "The tag from VERSION file already exists. Please update the VERSION file.",
-        # 8. Close the PR having DO NOT MERGE LABEL
-        "label" : "Please remove DO NOT MERGE LABEL",
-        # 9. message need to be placed here
+        "stale_label": 'This PR is stale because it has been open 15 days with no activity. Remove stale label or comment/update PR otherwise this will be closed in next 2 days.',
+        "stale_days": 15,
+        "stale_close_days": 2,
+        "staled_PR_closing": 'This PR was closed because it has been stalled for 2 days with no activity.',
+        "check_PR_target": 'Do not accept PR target from feature branch to master branch.',
+        "check_description": 'No Description on PR body. Please add valid description.',
+        "approve_merge": 'Pull Request Approved and Merged!',
+        "approve_comment": 'This pull request was approved and merged because of a slash command.',
+        "closing_comment": 'This pull request was closed because of a slash command.',
+        "check_version_file": 'The VERSION file exists. All ok',
+        "version_file_inexistence": "The VERSION file does not exist. Closing this pull request.",
+        "tagcheck_success": "The VERSION didn't match with tag. All ok",
+        "tagcheck_reject": "The tag from VERSION file already exists. Please update the VERSION file.",
+        "label": "Please remove DO NOT MERGE LABEL",
     }
     
-
+    # Define default messages  based on events
     if pr:
         msg["default"] = f"An Event is created on PR:\nTitle: {pr.title}\nURL: {pr.html_url}"
         msg["opened"] = f"New Pull Request Created by {pr.user.login}:\nTitle: {pr.title}\nURL: {pr.html_url}"
         msg["edited"] = f"Pull Request Edited by {pr.user.login}:\nTitle: {pr.title}\nURL: {pr.html_url}"
         msg["closed"] = f"Pull Request Closed by {pr.user.login}:\nTitle: {pr.title}\nURL: {pr.html_url}"
         msg["reopened"] = f"Pull Request Reopened by {pr.user.login}:\nTitle: {pr.title}\nURL: {pr.html_url}"
-
-
-    print("repo:",repo)
-    print("pulls:",pulls)
-    print("Hello")
-    # 1.Add "Stale" label to the PR if no active from 15 days
-    now = datetime.now()
-
-    for pull in pulls:
         
-        time_diff = now - pull.updated_at
-        # 1. Check if the time difference is greater than the stale_days
-        if time_diff > timedelta(days=msg.get("stale_days")):
-            print(f"Pull request: {pull.number} is stale!")
-            pull.create_issue_comment( msg.get("stale_label") )
-            pull.add_to_labels('Stale')
-
-        # 2. Close staled PR if 2 days of no activity
-        if "Stale" in [label.name for label in pull.labels]:
-            # check if the time difference is greater than the stale_close_days
-            if time_diff > timedelta(days=msg.get("stale_close_days")):
-                print(f"Pull request: {pull.number} is stale and closed!")
-                print(msg.get("staled_PR_closing"))
-                pull.edit(state="closed")
-                pull.create_issue_comment(msg.get("staled_PR_closing") )
-
-        # 3.Check if the pull request targets the master branch directly
-        if pull.base.ref == 'master' and not pull.head.ref.startswith('release/'):
-            print(f"Pull request: {pull.number} was targeted to master")
-            print(msg.get("check_PR_target"))
-            pull.edit(state='closed')
-            pull.create_issue_comment(msg.get("check_PR_target") )
-
-        # 4.Check if the pull request has a description
-        if not pull.body:
-            print(f"Pull request: {pull.number} has no description" )
-            pull.edit(state='closed')
-            pull.create_issue_comment(msg.get("check_description"))
-            print(msg.get("check_description"))
-
-    # 5_1 Check if the Approved comment in the pull request comments
-    if MERGE_PR.__eq__('true'):
-        if pr:    
-            pr.merge(merge_method = 'merge', commit_message = msg.get("approve_merge"))
-            pr.create_issue_comment(msg.get("approve_comment"))
-            print(msg.get("approve_comment"))
-
-    # 5_2 Check if the Close comment in the pull request comments
-    if CLOSE_PR.__eq__('true'):
-        if pr:            
-            pr.edit(state="closed")
-            pr.create_issue_comment(msg.get("closing_comment"))
-            print(msg.get("closing_comment"))
-
-    # 6. Check All the files and see if there is a file named "VERSION"
-    if pr:        
-        files = pr.get_files()
-        print(files)
-        version_file_exist = False
-        for file in files:
-            if file.filename == 'VERSION':
-                print(f"file : {file}")
-                version_file_exist = True
-                break
-        if version_file_exist:
-            print(msg.get("check_version_file") )
+    # Get current datetime
+    now = datetime.now()
+    
+    # Check events based on the workflow type
+    if  EVENT_CHECK =='stale' :
+        # 1. Add "Stale" label to the PR if no activity for 15 days
+        for pull in pulls:
+            time_diff = now - pull.updated_at
+            if time_diff > timedelta(days=msg.get("stale_days")):
+                pull.create_issue_comment(msg.get("stale_label"))
+                pull.add_to_labels('Stale')
+                GCHAT_MESSAGE.append(msg.get("stale_label"))
+        
+        # 2. Close stalled PR if no activity for 2 days
+        for pull in pulls:
+            if "Stale" in [label.name for label in pull.labels]:
+                if time_diff > timedelta(days=msg.get("stale_close_days")):
+                    pull.edit(state="closed")
+                    pull.create_issue_comment(msg.get("staled_PR_closing"))
+                    GCHAT_MESSAGE.append(msg.get("staled_PR_closing"))
+                    
+    if EVENT_CHECK =='pull':
+        # 3. Check if the PR targets the master branch directly
+        for pull in pulls:
+            if pull.base.ref == 'master' and not pull.head.ref.startswith('release/'):
+                pull.edit(state='closed')
+                pull.create_issue_comment(msg.get("check_PR_target"))
+                GCHAT_MESSAGE.append(msg.get("check_PR_target"))
+        
+        # 4. Check if the PR has a description
+        for pull in pulls:
+            if not pull.body:
+                pull.edit(state='closed')
+                pull.create_issue_comment(msg.get("check_description"))
+                GCHAT_MESSAGE.append(msg.get("check_description"))
+                
+        # 5. Check if the version from "VERSION" file exists as a tag
+        if pr and VERSION_FILE:    
+            tags = repo.get_tags()
+            tag_exist = False
+            for tag in tags:
+                if tag.name == VERSION_FILE:
+                    tag_exist = True
+                    break
+            if not tag_exist:
+                print(msg.get("tagcheck_success"))
+            else:
+                pr.create_issue_comment(msg.get("tagcheck_reject"))
+                pr.edit(state='closed')
+                GCHAT_MESSAGE.append(msg.get("tagcheck_reject"))
         else:
-            pr.create_issue_comment(msg.get("version_file_inexistence") )
-            print(msg.get("version_file_inexistence"))
+            pr.create_issue_comment(msg.get("version_file_inexistence"))
             pr.edit(state='closed')
+            GCHAT_MESSAGE.append(msg.get("version_file_inexistence"))
 
-    # 7. Check if version name from "VERSION" already exists as tag   
-    if pr and VERSION_FILE:    
-        print(f"version from VERSION_FILE : {VERSION_FILE}")
-        tags = repo.get_tags()
-        tag_exist = False
-        for tag in tags:
-            if tag.name == VERSION_FILE:
-                print(f"tag : {tag.name}")
-                tag_exist = True
-                break
-        if not tag_exist:
-            print(msg.get("tagcheck_success") )
-        else:
-            pr.create_issue_comment(msg.get("tagcheck_reject") )
-            print(msg.get("tagcheck_reject") )
-            pr.edit(state='closed')
+        # 6. Close PRs with DO NOT MERGE label
+        if pr:
+            labels = pr.get_labels()
+            if "DO NOT MERGE" in [label.name for label in labels]:
+                pr.edit(state='closed')
+                pr.create_issue_comment(msg.get("label"))
+                GCHAT_MESSAGE.append(msg.get("label"))
 
-    # 8. Do not merge PR message and close the PR
-    if pr:
-        labels = pr.get_labels()
-        print(pr)
-        print(pr_number)
-        print(labels)
-        if "DO NOT MERGE" in [label.name for label in labels]:
-            pr.edit(state='closed')
-            pr.create_issue_comment(msg.get("label"))
-            print(msg.get("label"))        
+    if EVENT_CHECK =='slash':
+        # 7.1 Check if the Approved comment is in the PR comments
+        if MERGE_PR == 'true':
+            if pr:    
+                pr.merge(merge_method='merge', commit_message=msg.get("approve_merge"))
+                pr.create_issue_comment(msg.get("approve_comment"))
+                GCHAT_MESSAGE.append(msg.get("approve_comment"))
+    
+        # 7.2 Check if the Close comment is in the PR comments
+        if CLOSE_PR == 'true':
+            if pr:            
+                pr.edit(state="closed")
+                pr.create_issue_comment(msg.get("closing_comment"))
+                GCHAT_MESSAGE.append(msg.get("closing_comment"))
 
-    # 9. Google chat integration with github
-    print(f"event vale ={EVENT}")
-    print(f"GCHAT_WEBHOOK_URL: ={GCHAT_WEBHOOK_URL:}")
-
+    # 8. Google Chat integration with GitHub
     if EVENT and GCHAT_WEBHOOK_URL:
         message = msg.get("default")
         message = msg.get(EVENT, message)
-        issue = repo.get_issue(number=pr)
-        comments = issue.get_comments()
-        if comments:
-                for comment in comments:
-                    message += f"\n Author: {comment.user.login}\n"
-                    message += f"  Comment: {comment.body}\n\n"
-            
-
-        payload = {
-            "text" : message
-        }
-        
-        response = requests.post(GCHAT_WEBHOOK_URL, json=payload)
-        #res = requests.post(GCHAT_WEBHOOK_URL, json={'text': message})
-        #print(res.json())
-        #print(response)
-        #print(EVENT)
-        print("hello from docker chat")
+        for n in GCHAT_MESSAGE:
+            message =message +'\nIssue comment : ' + n
+        response = send_message_to_google_chat(message, GCHAT_WEBHOOK_URL)
 
 except Exception as e:
-    print(f"Failed to run the job. exception: {str(e)}")      
+    print(f"Failed to run the job. Exception: {str(e)}")
